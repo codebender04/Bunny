75574ea5ae19b5de6ec400d5d05ceb69713f33c2
codebender <khoa28122005@gmail.com>
2024-09-23 23:05:42 +0700

    Add Shake

--- Assets/_Game/Scripts/CharacterMovement.cs
+++ Assets/_Game/Scripts/CharacterMovement.cs
@@ -4,22 +4,32 @@ using UnityEngine.Tilemaps;
 using System.Collections.Generic;
 using System.Collections;
 using static GameInput;
+using Unity.VisualScripting;
 
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float jumpHeight = 2f;
     [SerializeField] private float jumpDuration = 0.5f; 
+    [SerializeField] private float shakeDuration = 0.5f;
+    [SerializeField] private float shakeStrengh = 0.1f;
+    [SerializeField] private int shakeVibrato = 10;
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private GameInput gameInput;
+
+    private static HashSet<Vector3Int> occupiedCells = new HashSet<Vector3Int>();
 
     private Queue<Vector2> movementQueue = new Queue<Vector2>();
     private bool jumpCompleted = false;
     private bool isExecutingMovements;
     private bool isActivated = true;
     private Vector2 targetPosition;
+    private Vector3Int currentCell;
     private void Start()
     {
         gameInput.OnMovementKeyPressed += GameInput_OnMovementKeyPressed;
+
+        currentCell = tilemap.WorldToCell(transform.position);
+        occupiedCells.Add(currentCell);
     }
 
     private void GameInput_OnMovementKeyPressed(object sender, OnMovementKeyPressedEventArgs e)
@@ -30,10 +40,15 @@ public class CharacterMovement : MonoBehaviour
     private IEnumerator JumpToTile(Vector2 direction)
     {
         jumpCompleted = false;
+        currentCell = tilemap.WorldToCell(transform.position);
         Vector2 targetPosition = (Vector2)transform.position + direction;
+        Vector3Int targetCell = tilemap.WorldToCell(targetPosition);
 
-        if (tilemap.HasTile(tilemap.WorldToCell(targetPosition)))
+        if (!occupiedCells.Contains(targetCell) && tilemap.HasTile(targetCell))
         {
+            occupiedCells.Add(targetCell);
+            occupiedCells.Remove(currentCell);
+
             transform.DOJump(targetPosition, jumpHeight, 1, jumpDuration).OnComplete(() =>
             {
                 jumpCompleted = true;
@@ -41,7 +56,13 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
-            jumpCompleted = true;
+            Vector3 originalPosition = transform.position;
+            transform.DOShakePosition(shakeDuration, strength: new Vector3(shakeStrengh, shakeStrengh, 0), shakeVibrato, randomness: 90, snapping: false, fadeOut: true, ShakeRandomnessMode.Harmonic)
+                .OnComplete(() =>
+                {
+                    transform.position = originalPosition;
+                    jumpCompleted = true;
+                });
         }
         while (!jumpCompleted)
         {
